fb46405c80fdae90daa48d93b1724f057dea35d6
jmcahal <65468538+jmcahal@users.noreply.github.com>
2020-06-05 12:29:14 -0400

    updated with beginning, end, and styling

--- connect4.js
+++ connect4.js
@@ -5,12 +5,17 @@
  * board fills (tie)
  */
 
-
 const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
-const board = []; // array of rows, each row is array of cells  (board[y][x])
+let board = []; // array of rows, each row is array of cells  (board[y][x])
+
+// if ( navigator.platform.indexOf('Win') != -1 ) {
+//   window.document.getElementById("wrapper").setAttribute("class", "windows");
+// } else if ( navigator.platform.indexOf('Mac') != -1 ) {
+//   window.document.getElementById("wrapper").setAttribute("class", "mac");
+// }
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -24,10 +29,10 @@ function makeBoard() {
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
-
+const htmlBoard = document.querySelector("#board")
 function makeHtmlBoard() {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-const htmlBoard = document.querySelector("#board")
+//const htmlBoard = document.querySelector("#board")
   // TODO: add comment for this code
   // create the table row
   let top = document.createElement("tr");
@@ -36,6 +41,10 @@ const htmlBoard = document.querySelector("#board")
   // add an event listener for when the top of a column is clicked it will drop the item to the next available cell in relation to the bottom.
   top.addEventListener("click", handleClick);
   // loop over the columns giving the top cell an id of 0-6 left to right
+ 
+  // indicate the which player's turn it is
+  const playerTurn = document.getElementById('playerTurn')
+  playerTurn.innerText = `Player ${currPlayer}`;
 
   for (let x = 0; x < WIDTH; x++) {
     //create a data container for each top cell
@@ -47,7 +56,7 @@ const htmlBoard = document.querySelector("#board")
       else {this.style.backgroundColor = "red"}
     })
     headCell.addEventListener("mouseleave", function(){
-      this.style.backgroundColor = "white"
+      this.style.backgroundColor = "black"
     })
     headCell.setAttribute("id", x);
     top.append(headCell);
@@ -68,7 +77,6 @@ const htmlBoard = document.querySelector("#board")
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
-
 function findSpotForCol(x) {
   // TODO: write the real version of this, rather than always returning 0
   for (let y = HEIGHT - 1; y >= 0; y--) {
@@ -79,10 +87,7 @@ function findSpotForCol(x) {
   return null;
 }
 
-
-
 /** placeInTable: update DOM to place piece into HTML table of board */
-
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
   const gamePiece = document.createElement("div");
@@ -110,8 +115,14 @@ function endGame(msg) {
 }
 
 /** handleClick: handle click of column top to play piece */
-
 function handleClick(evt) {
+  //  indicate which player's turn it is. 
+  let player = () =>{if (currPlayer ===1){
+    return 2}
+    else { return 1}
+  }
+  playerTurn.innerText = `Player ${player()}`;
+
   // get x from ID of clicked cell
   let x = +evt.target.id;
 
@@ -128,23 +139,25 @@ function handleClick(evt) {
 
   // check for win
   if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
+    const winner = document.getElementById("winner");
+    winner.innerText = `Player ${currPlayer} wins!`;
+    popUp.style.display = "block";
   }
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
   if (board.every(row => row.every(cell => cell))){
-    return endGame("Tie!")
+    const winner = document.getElementById("winner");
+    winner.innerText = `Tie! Wow, what a game!`;
+    popUp.style.display = "block";
   }
 
   // switch players
   // TODO: switch currPlayer 1 <-> 2  
   currPlayer = currPlayer === 1 ? 2 : 1;
-
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
-
 function checkForWin() {
   function _win(cells) {
     // Check four cells to see if they're all color of current player
@@ -181,5 +194,52 @@ function checkForWin() {
   }
 }
 
-makeBoard();
-makeHtmlBoard();
+//function for when the start button is clicked
+const startGame =() =>{
+  startButton.remove();
+  makeBoard();
+  makeHtmlBoard();
+}
+
+//create startButton
+const startButton = document.createElement("button");
+startButton.addEventListener("click",startGame);
+    startButton.type = "Button";
+    startButton.setAttribute("id", "start")
+  startButton.innerText= "Start Game!";
+  document.body.append(startButton);
+
+// Get the modal
+const popUp = document.getElementById("overModal");
+
+// Get the <span> element that closes the modal
+const close = document.getElementsByClassName("close")[0];
+
+// Get the play again button
+const playAgain = document.getElementById("play");
+
+playAgain.addEventListener("click",function(){
+  htmlBoard.innerHTML = '';
+  board = [];
+  popUp.style.display = "none";
+  currPlayer = 1
+  playerTurn.innerText = `Player ${currPlayer}`;
+  makeBoard();
+  makeHtmlBoard();
+});
+
+const closePopUp = function(){
+  popUp.style.display = "none";
+  htmlBoard.innerHTML = '';
+  playerTurn.innerText = "";
+  board = [];
+  const startButton = document.createElement("button");
+  startButton.addEventListener("click",startGame);
+    startButton.type = "Button";
+    startButton.setAttribute("id", "start")
+  startButton.innerText= "Start Game!";
+  document.body.append(startButton);
+};
+
+close.addEventListener("click",closePopUp);
+
